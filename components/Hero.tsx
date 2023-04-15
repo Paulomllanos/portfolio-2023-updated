fbf495f9e07796f27a151246c0597022b8ae149f
Paulo Maldonado <paulomllanos@gmail.com>
2023-04-14 20:21:54 -0400

    Hero listo y header igual, empezando about

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
+
 
 type Props = {}
 // Hero 44:40
 export default function Hero({}: Props) {
+
+  const [text, count] = useTypewriter(
+    {
+      words: ["Hi, my name is Paulo Maldonado", "I'm Full Stack Developer"],
+      loop: true,
+      delaySpeed: 2000
+    }
+  );
+
+
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col mt-32 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles />
+      <img className='relative rounded-full h-32 w-32 mx-auto object-cover mt-6' src="https://images.unsplash.com/photo-1565110426846-2f694d651aaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80" alt="" />
+      <div className='z-20 mt-5'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#F7AB0A'/>
+        </h1>
+
+        <div className='pt-10'>
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import { NextPage } from 'next'
 import  Header  from "../components/Header"
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 
 
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Paulo Maldonado</title>
       </Head>
@@ -15,10 +16,14 @@ const Home: NextPage = () => {
       {/* Headers */}
       <Header />
       {/* Hero */}
-      <section id='hero'>
+      <section id='hero' className="snap-start">
         <Hero />
       </section>
+
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
       {/* Skills */}
       {/* Projects */}
       {/* Contact Me */}
